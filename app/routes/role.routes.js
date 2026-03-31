e2c1b0d44e3cebe51bda0811faa279a5e4aeebe3
Jonathan Muhire <muhirejonathan123@gmail.com>
2026-03-31 15:38:56 -0500

    chore: restore local backend WIP from stash (raw snapshot)

--- app/routes/role.routes.js
+++ app/routes/role.routes.js
@@ -1,22 +1,26 @@
-
 import { Router } from "express";
 import authenticate from "../authorization/authorization.js";
 import {
   getAllRoles,
   getRoleById,
-  updateUserRole
+  updateUserRole,
+  createRole,
+  updateRole,
+  deleteRole
 } from "../controllers/role.controller.js";
+import { verifyToken } from "../middleware/authJwt.js";
+import requireManager from "../authorization/requireManager.js";
 
 const router = Router();
 
-// Get all roles
+// Public routes (with authenticate)
 router.get("/", authenticate, getAllRoles);
-
-// Get single role by ID
 router.get("/:id", authenticate, getRoleById);
-
-// Update user role
 router.put("/update-user", authenticate, updateUserRole);
 
+// Manager routes
+router.post("/", [verifyToken, requireManager], createRole);
+router.put("/:id", [verifyToken, requireManager], updateRole);
+router.delete("/:id", [verifyToken, requireManager], deleteRole);
 
 export default router;
